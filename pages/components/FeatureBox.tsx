17b1e7a802bb43f12c4667f0690add0f90755f8e
origamium <onoueorigami@gmail.com>
2021-05-10 00:27:38 +0900

    はい

--- pages/components/FeatureBox.tsx
+++ pages/components/FeatureBox.tsx
@@ -7,7 +7,7 @@ export type FeatureBoxProps = {
 
 export const FeatureBox: React.FC<FeatureBoxProps> = ({ title, text }) => {
     return (
-        <article className={"flex flex-col w-52 border-4 p-4"}>
+        <article className={"flex flex-col w-60 md:w-52 border-4 rounded-md border-pink-200  p-4 bg-pink-100"}>
             <h4 className="text-xl font-bold text-center pb-4">{title}</h4>
             <p>{text}</p>
         </article>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,30 @@ import { FeatureBox } from "./components/FeatureBox";
 
 const Index = () => {
     return (
-        <div>
+        <div className="bg-pink-50">
             <header>
                 <div className="flex justify-center p-4">
                     <Image src="/stab-wounds.svg" width={140} height={"140"} />
                     <div className="sm:max-w-xs md:max-w-max pl-4">
                         <h1 className="text-6xl font-bold md:pt-8">Killify</h1>
-                        <h2 className="text-2xl py-2">セキュアでスケーラブルなフルマネージドキルサービス</h2>
+                        <h2 className="text-2xl py-2">セキュアでスケーラブルなフルマネージド暴力サービス</h2>
                     </div>
                 </div>
             </header>
             <main className="flex flex-col gap-4 items-center w-screen">
                 <h3 className="text-2xl font-bold">Features</h3>
                 <p className="text-3xl text-center">
-                    コンピューター上のタスクキルから野犬の安楽死 <br />
-                    見せしめから事故に見せかけた殺人をどこからでも簡単に実行
+                    胎児の中絶から愛犬の安楽死 <br />
+                    事故に見せかけた殺人をどこからでも簡単に実行
                 </p>
 
                 <div>{/* country */}</div>
 
-                <div className="flex justify-around gap-3">
+                <div className="flex justify-around gap-3 flex-col md:flex-row">
+                    <FeatureBox
+                        title={"セキュア"}
+                        text={`我々はお客様のデータを安全に保つため、弊社データセンターのすべてのサーバーは侵入者が発生した場合において自爆するように設計されました。`}
+                    />
                     <FeatureBox
                         title={"誰でも利用可能"}
                         text={`我々はお客様を差別することはいたしません。カルテルやISIL、や共産党幹部、ダウン症の未就学児、統合失調症患者にもお使いいただけます。`}
@@ -38,6 +42,7 @@ const Index = () => {
                         text={`お客様の利用状態によって刑事責任が発生する場合においてもkillifyでは刑事免責保証のサポートにより、お客様を安全な状態に保ちます。`}
                     />
                 </div>
+                {/* case study*/}
                 <h3 className="text-2xl text-center">Pricing</h3>
                 <div>
                     <article>
