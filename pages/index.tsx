e5aafe464d27143660d274d2d88f40201d975dca
ONOUE Origami <onoueorigami@gmail.com>
2021-05-09 17:35:15 +0900

    はい

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { css } from "@emotion/react";
 
 export default function Home() {
     return (
-        <div className="relative">
+        <div className="">
             <header
                 className="p-1 flex justify-around"
                 css={css`
@@ -13,7 +13,7 @@ export default function Home() {
             >
                 <div>
                     <h1 className="text-4xl">Killify</h1>{" "}
-                    <h2 className="text-2xl">セキュアでスケーリングが容易なフルマネージド型killサービス</h2>{" "}
+                    <h2 className="text-2xl">セキュアでスケーリングが容易なフルマネージド型killサービス</h2>
                 </div>
                 <div>
                     <Image
@@ -26,7 +26,27 @@ export default function Home() {
                     />
                 </div>
             </header>
-            <main></main>
+            <main>
+                <h3 className="text-2xl text-center">Features</h3>
+                <div>
+                    <article>
+                        <h4 className="text-xl">ANYTHING</h4>
+                        <p>コンピュータ上のプロセスのkillから獣類の間引き、殺人までサポートします。</p>
+                    </article>
+                    <article>
+                        <h4 className="text-xl">EVERYONE</h4>
+                        <p>人種、宗教、年齢、所属組織に関係なく即座にkillサービス展開が可能。</p>
+                    </article>
+                    <article>
+                        <h4 className="text-xl">ELASTIC</h4>
+                        <p>1件の殺人から大規模虐殺までのkillタスクのスケーリングが可能。</p>
+                    </article>
+                    <article>
+                        <h4 className="text-xl">IMMUNITY</h4>
+                        <p>killifyではすべてのプランにおいて、すべての国において刑事免責保証が適用可能です。</p>
+                    </article>
+                </div>
+            </main>
         </div>
     );
 }
